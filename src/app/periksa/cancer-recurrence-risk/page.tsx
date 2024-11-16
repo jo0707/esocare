a1fa6c1fb6a474431c09e9717c800eaeca59796c
jo0707 <josua123690707@gmail.com>
2024-11-16 14:13:44 +0700

    fix: save patient result

--- src/app/periksa/cancer-recurrence-risk/page.tsx
+++ src/app/periksa/cancer-recurrence-risk/page.tsx
@@ -154,7 +154,7 @@ export default function CancerRecurrenceOutcomeCheck() {
     }
 
     function save() {
-        savePatientResult(patient)
+        if (patient) savePatientResult(patient)
         router.push("/dashboard")
     }
 
